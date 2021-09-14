ae3c5cd7bf834abb44e137fb98e41c147cc93974
cdirks4 <cdirks4@me.com>
2021-09-14 09:30:01 -0400

    changing to crypto app

---
+++ models/Portfolio.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Coin = new Schema(
+	{
+		title: String,
+		amount: Number,
+		ppc: Number,
+		gain: Number,
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const Portfolio = new Schema(
+	{
+		owner: String,
+		coins: [Coin],
+	},
+	{
+		timestamps: true,
+	}
+);
+
+module.exports = mongoose.model('Post', PostSchema);
